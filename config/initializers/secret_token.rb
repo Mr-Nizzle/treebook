75560f42b9a0f55b6e81d42e60622bbf92ac142a
Ricardo Guillen <nizzle@nizzledev.com>
2014-06-30 22:52:07 -0600

    Nizzle: initial commit. Statuses updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '868e13a99b02b28a2b48345b51c2fb5c8d1275116f54644a0f38b8b693b5958bda7026f155b5478bae401de7c5ad9802ccb09eaab22b5b79d24a478dc5d11161'
