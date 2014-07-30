c508d6bfedd02e893f16df63192fe097b77e2a22
Ricardo Guillen <nizzl@nizzledev.com>
2014-07-29 23:58:30 -0600

    Nizzle: test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :profile_name, :presence => true
+
    def full_name
    	"#{first_name} #{last_name}"
    end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,25 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+    assert !user.errors[:last_name].empty?
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user shouwld have a unique profile name" do
+    user = User.new
+    user.first_name = "Ricardo"
+    user.last_name = "Guillen"
+    user.email = "ricardo@503estudio.com"
+    user.password = "W3lcom3321"
+    user.password_confirmation = "W3lcom3321"
+    user.profile_name = "nizzle"
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
 end
